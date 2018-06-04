79be69d37f6db535d8a449d47806fa2a18cb4f41
Arun Thayanithy <arunthaya@gmail.com>
2018-06-04 10:55:12 -0400

    Added add db functionality

--- src/main/java/com/example/springbootwithreactjs/database/MongoDB.java
+++ src/main/java/com/example/springbootwithreactjs/database/MongoDB.java
@@ -1,5 +1,6 @@
 package com.example.springbootwithreactjs.database;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
@@ -13,6 +14,7 @@ public class MongoDB {
     //Key strings used for database purposes
     public static final String DB_NAME = "pageparser";
     public static final String COLLECTION_NAME = "roughparse";
+    public static final String URL = "url";
 
     private DB db;
     private DBCollection coll;
@@ -32,6 +34,12 @@ public class MongoDB {
         Mongo mongo = new Mongo("localhost, 27017");
         db = mongo.getDB(DB_NAME);
         coll = db.getCollection(COLLECTION_NAME);
+    }
+
+    public void add(String page){
+        BasicDBObject temp = new BasicDBObject();
+        temp.append(URL, page);
+        coll.insert(temp);
     }
 
     /*Have to add logic where if it is a about page, has a location, or has pictures
