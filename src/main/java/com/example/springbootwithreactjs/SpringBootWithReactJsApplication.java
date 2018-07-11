3c16e9628e30a0c73faaa109c6d1f33543558a00
Arun Thayanithy <arunthaya@gmail.com>
2018-07-11 17:48:36 -0400

    Postgres Heroku Integration

--- src/main/java/com/example/springbootwithreactjs/SpringBootWithReactJsApplication.java
+++ src/main/java/com/example/springbootwithreactjs/SpringBootWithReactJsApplication.java
@@ -8,12 +8,31 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @SpringBootApplication
 public class SpringBootWithReactJsApplication {
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithReactJsApplication.class, args);
+		try {
+			getConnection();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		System.out.println("-------------------------->");
+		System.out.println("The JDBC_DATABASE_URL is: "+dbUrl);
+		System.out.println("<--------------------------");
+		return DriverManager.getConnection(dbUrl);
+	}
 }
