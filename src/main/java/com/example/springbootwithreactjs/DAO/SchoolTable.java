aec343103f56432c9d5af533e71cb03db4b937b1
arunthaya@gmail.com <arunthaya@gmail.com>
2018-07-13 21:21:27 -0400

    Preliminary Database Schema

--- src/main/java/com/example/springbootwithreactjs/DAO/SchoolTable.java
+++ src/main/java/com/example/springbootwithreactjs/DAO/SchoolTable.java
@@ -10,7 +10,45 @@ public class SchoolTable implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long _id;
+
+    @Column(name = "school", nullable = false)
     private String schoolName;
 
+    @Column(name = "location", nullable = false)
+    private String schoolLocation;
 
+    @Column(name = "about", nullable = false)
+    private String aboutParagraph;
+
+    public Long get_id() {
+        return _id;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public String getSchoolLocation() {
+        return schoolLocation;
+    }
+
+    public String getAboutParagraph() {
+        return aboutParagraph;
+    }
+
+    public void set_id(Long _id) {
+        this._id = _id;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public void setSchoolLocation(String schoolLocation) {
+        this.schoolLocation = schoolLocation;
+    }
+
+    public void setAboutParagraph(String aboutParagraph) {
+        this.aboutParagraph = aboutParagraph;
+    }
 }
